ba9f226af6b3cd2cbfc9c23afc60814876d92233
Peter Shen <peterchenshen@gmail.com>
2020-01-09 16:54:07 +0800

    test

--- mkfiles/makingtests.mk
+++ mkfiles/makingtests.mk
@@ -1,6 +1,26 @@
 SHELL:=/bin/bash
 
+# find the root path
 export TESTROOT := $(shell pwd -P | sed -e 's,/LTF/.*,/LTF,')
 
-testconfig:
-	@defaultgoal-testconfig
+# export some useful tools
+export TESTANALYZER ?= $(TESTROOT)/analyzers/test-analyzer
+
+# export test config file
+export TESTCONFIG ?= testconfig.mk
+ifneq ($(shell echo $(TESTCONFIG) | cut -c1),/)
+TESTCONFIG:=$(shell echo "$$(pwd)/$(TESTCONFIG)")
+endif
+ifeq ($(shell test -e $(TESTCONFIG) && echo 0),0)
+include $(TESTCONFIG)
+endif
+
+# add new search path
+ifneq ($(TEST_PATHPREFIX),)
+PATH:=$(TEST_PATHPREFIX):$(TESTROOT)/bin:$(PATH)
+else
+PATH:=$(TESTROOT)/bin:$(PATH)
+endif
+
+.DEFAULT:
+	@PATH="$(PATH)" defaultgoal $@
