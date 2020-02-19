e1946de635951a62c50a7aa40f0faaa584ef84e7
Peter Shen <peterchenshen@gmail.com>
2020-02-19 17:31:58 +0800

    gitignore

--- tests/example/hello/host_hello.sh
+++ tests/example/hello/host_hello.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+echo "running host test ..."
 
-echo "hello"
+compile "hello.c"
+putfile "__hello"
+invoke "chmod +x __hello && ./__hello > __tmp.log"
+getfile "__tmp.log"
+cat "__tmp.log"
+path="$(invoke "find /tmp/testhelper* -name __tmp.log")"
+getfile_ssh "$path" "__tmp.log.1"
+cat "__tmp.log.1"
+
+invoke "ls 123"
+echo "result:$?"
