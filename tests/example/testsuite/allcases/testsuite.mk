b9f962085bb7df298298067ccda40efaf50b5ee9
Peter Shen <peterchenshen@gmail.com>
2020-04-07 14:26:33 +0800

    add a testsuite to run all cases

---
+++ tests/example/testsuite/allcases/testsuite.mk
@@ -0,0 +1,5 @@
+TESTSUITE_TESTS+=/opt/share/LTF/tests/example/hello_c
+TESTSUITE_TESTS+=/opt/share/LTF/tests/example/hello_script
+TESTSUITE_TESTS+=/opt/share/LTF/tests/example/optfile
+TESTSUITE_TESTS+=/opt/share/LTF/tests/example/reboot
+TESTSUITE_TESTS+=/opt/share/LTF/tests/example/switch
