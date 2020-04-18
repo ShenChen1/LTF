4b1c24e7694016f52596713f676ad99de4250e0d
Peter Shen <peterchenshen@gmail.com>
2020-04-18 14:00:40 +0800

    fix unbound issue

--- tests/example/optfile/host_optfile.sh
+++ tests/example/optfile/host_optfile.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 file="/tmp/hosts"
-cp /etc/hosts $file
+
+method=""
 
 echo "test case 1"
-putfile "$file" __tmp
-getfile __tmp
+"putfile${method}" "$file" __tmp
+"getfile${method}" __tmp
 diff __tmp "$file"
 
 echo "test case 2"
-putfile "$file"
-getfile "${file##*/}" __tmp1
+"putfile${method}" "$file"
+"getfile${method}" "${file##*/}" __tmp1
 diff __tmp1 "$file"
 
 echo "test case 3"
-putfile "$file" "$file.tmp"
-getfile "$file.tmp" __tmp2
+"putfile${method}" "$file" "$file.tmp"
+"getfile${method}" "$file.tmp" __tmp2
 diff __tmp2 "$file"
 
 echo "test failure 1"
-putfile "$file" "./nodir/"
+"putfile${method}" "$file" "./nodir/"
+echo "result:$?"
 
 echo "test failure 2"
-putfile "nofile" "./nodir/"
+"putfile${method}" "nofile" "./nodir/"
+echo "result:$?"
 
 exit 0
