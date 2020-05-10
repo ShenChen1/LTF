35d0be4a3a39a43426413dcdf963d273988061c3
Peter Shen <peterchenshen@gmail.com>
2020-05-10 09:42:45 +0800

    update coredump case

--- tests/example/coredump/host_test.sh
+++ tests/example/coredump/host_test.sh
@@ -3,6 +3,9 @@
 invoke "echo 'running on' $(whoami)"
 
 $(${TESTANALYZER} arch)-gcc -g -o demo -fomit-frame-pointer -fno-unwind-tables -fno-asynchronous-unwind-tables demo.c
+$(${TESTANALYZER} arch)-objcopy --only-keep-debug demo demo.dbg
+$(${TESTANALYZER} arch)-objcopy --strip-debug demo
+$(${TESTANALYZER} arch)-objcopy --add-gnu-debuglink=demo.dbg demo
 putfile "demo"
 
 invoke "echo core > /proc/sys/kernel/core_pattern"
